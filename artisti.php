bd1eaf0f6ce9bd871c58b5e7e841a05903e477fa
seldo3106 <mariomariomrm91@gmail.com>
2026-01-18 14:50:05 +0100

    Prima aggiunta

---
+++ artisti.php
@@ -0,0 +1,121 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "cinzofi";
+
+$connessione = new mysqli($host,$user,$password,$database);
+
+if($connessione == false){
+    die("Errore di connessione: " . $connessione->connect_error);
+}
+
+$id = $_GET['id']; 
+
+$sql = "SELECT * FROM artista WHERE idartista = " . $id;
+$richiesta = $connessione->query($sql);
+$artista = $richiesta->fetch_array();
+
+//$idalbum = $connessione->query("SELECT * FROM cantanti_album WHERE idartista = " . $id);
+$album = $connessione->query("SELECT * FROM album INNER JOIN cantanti_album ON album.idalbum = cantanti_album.idalbum WHERE cantanti_album.idartista = ". $id . " ORDER BY data_pub DESC");
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?= $artista['nome'] ?></title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="artista.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<a href="home.html">
+			<div id="indietro">
+				<img src="asset/left-arrow.png" height="75px">
+			</div>
+		</a>
+		<div class="barra">
+			<h1><?= $artista['nome'] ?></h1>
+			<div id="info">
+				<!-- <img src="copertine/<?= $artista['nome'] ?>/<?= $artista['nome'] ?>.gif"> -->
+				<div id="testo">
+					<p><?= $artista['descrizione'] ?></p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="corpo">
+            <?php 
+            while($tempora = $album->fetch_array()){	//Ciclo per gli album
+				//$idNumero = $tempora['idalbum'];
+
+				//Per vedere se è un album, mixtape o EP
+				switch($tempora['tipo']){
+					case 1:
+						$tipo = "Album";
+						break;
+					case 2:
+						$tipo = "Mixtape";
+						break;
+					case 3:
+						$tipo = "EP";
+						break;
+				}
+					
+				//Per evitare di avere problemi con il nome dell'immagine
+				$nomeImmagine = str_replace("?","",$tempora['nome']);
+
+				//Variabile dove viene scritto il testo html
+                $testoHTML = '
+            <div class="album">
+				<h3>' . $tempora['nome'] . ' - '. $tipo .'</h3>
+				<img src="copertine/'. $artista['nome'] .'/'. $nomeImmagine .'.png" height="400px">
+				<ol class="tracce">';
+
+				
+				//Variabile dove vengono salvati le ennuple che contengono i singoli dell'album
+				$tempo = $connessione->query("SELECT * FROM singolo WHERE idalbum = " . $tempora['idalbum'] . " ORDER BY num_traccia ASC");
+				//Ciclo per la lista delle canzoni
+				while($singolo = $tempo->fetch_array()){
+					$testoHTML .= '<li><a href="singolo.php?id=' . $singolo['idsingolo'] . '">'. $singolo['nome'] .'</a></li>';
+				}
+				$testoHTML .= '</ol></div>';
+
+				echo $testoHTML;
+            }
+            ?>
+		</div>
+		<div class="barraSin">
+			<h4>Singoli senza album</h4>
+			<div class="singoli">
+				<?php
+					$richiesta = $connessione->query("SELECT * FROM singolo INNER JOIN cantanti_singolo ON singolo.idsingolo = cantanti_singolo.idsingolo WHERE cantanti_singolo.idartista = ". $id . " AND singolo.idalbum IS NULL ORDER BY data_pub DESC");
+					while($singolo = $richiesta->fetch_array()){
+						$testoHTML = '
+				<a href="singolo.php?id='. $singolo['idsingolo'] .'"><div class="singolo">
+				<h3>'. $singolo['nome'] .'</h3>
+				<img src="copertine/'. $artista['nome'] .'/'. $singolo['nome'] .'.png">
+				</div></a>';
+
+						echo $testoHTML;
+					}
+				?>
+				<!-- 
+				<a href="./singoli/uNa_DiReZioNe_giUsTa.html"><div class="singolo">
+					<h3>uNa DiReZioNe giUsTa<br>(feat. Neffa)</h3>
+					<img src="./singoli/uNa_DiReZioNe_giUsTa.png">
+				</div></a>
+				<a href="./singoli/6itch.html"><div class="singolo">
+					<h3>6itch</h3>
+					<img src="./singoli/6itch.png">
+				</div></a> -->
+			</div>
+		</div>
+	</body>
+</html>
---
+++ singolo.php
@@ -0,0 +1,95 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "cinzofi";
+
+$connessione = new mysqli($host,$user,$password,$database);
+
+if($connessione == false){
+    die("Errore di connessione: " . $connessione->connect_error);
+}
+
+$id = $_GET['id'];
+
+//Salva la ennupla con le info di un singolo
+$temp = $connessione->query("SELECT * FROM singolo WHERE idsingolo = ". $id);
+$singolo = $temp->fetch_array();
+
+//Salva la ennupla con le info di un album
+$temp = $connessione->query("SELECT * FROM album WHERE idalbum = ". $singolo['idalbum']);
+$album = $temp->fetch_array();
+$nomeImmagine = str_replace("?","",$album['nome']);
+
+//Salva la ennupla con le info di chi l'ha cantato
+$idartisti = $connessione->query("SELECT * FROM cantanti_singolo WHERE idsingolo = ". $id);
+
+//Estrapola il nome di chi ha fatto la canzone per trovare la cartella dove è salvata la copertina dell'album o singolo
+$temp = $connessione->query("SELECT * FROM cantanti_singolo WHERE idsingolo = ". $id. " AND grado = 1");
+$idartista = $temp->fetch_array();
+$temp = $connessione->query("SELECT * FROM artista WHERE idartista = ". $idartista['idartista']);
+$artista = $temp->fetch_array();
+
+
+//Salva la ennupla con le info di chi l'ha prodotto
+$idprod = $connessione->query("SELECT * FROM prod_singolo WHERE idsingolo = ". $id);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= $singolo['nome'] ?></title>
+        <link rel="stylesheet" href="singolo.css">
+		<link rel="stylesheet" href="style_236451.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div class="barra" style="background-image: linear-gradient(#<?= $album['colore1'] ?>,#<?= $album['colore2'] ?>);">
+			<img src="copertine/<?= $artista['nome'] ?>/<?= $nomeImmagine ?>.png" width="300" height="300" id="copertina">
+			<div id="tedio">
+				<h1><?= $singolo['nome'] ?></h1>
+                <div id="info">
+                    <div class="dati">
+                        <h4 id="dati1">Prodotto da:</h4>
+                        <p id="info1"><?php
+                            $produttori = [];
+                            while($temp = $idprod->fetch_array()){
+                                $tempo = $connessione->query("SELECT * FROM artista WHERE idartista = ". $temp['idartista']);
+                                $prod = $tempo->fetch_array();
+                                $produttori[] = $prod['nome'];
+                            }
+                            echo implode(", ",$produttori);
+                        ?>
+                        </p>
+                    </div>
+                    <div class="dati">
+                        <h4 id="dati2">Cantato da:</h4>
+                        <p id="info2"><?php
+                            $cant = [];
+                            while($temp = $idartisti->fetch_array()){
+                                $tempo = $connessione->query("SELECT * FROM artista WHERE idartista = ". $temp['idartista']);
+                                $cantante = $tempo->fetch_array();
+                                $cant[] = $cantante['nome'];
+                            }
+                            echo implode(", ",$cant);
+                        ?></p>
+                    </div>
+                </div>
+				<a href="<?= $singolo['link'] ?>" id="collegamento">Collegamento alla canzone</a>
+			</div>
+		</div>
+        <a href="artisti.php?id=<?= $artista['idartista'] ?>">
+			<div id="indietro">
+				<img src="asset/left-arrow.png" height="75px">
+			</div>
+		</a>
+        <div id="testo">
+            <h3>Testo della canzone</h3>
+            <p><?= $singolo['testo'] ?></p>
+        </div>
+    </body>
+</html>
